a8ec71b40ee73a6dd7b980de2047164c14cbc5ef
Jan David Ella <117320525+JanDavidElla@users.noreply.github.com>
2026-08-25 14:56:39 -0700

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,54 @@
+class Solution:
+
+    """
+    Plan:
+
+    Before each word, add (number)#(word)
+    It will look at the number, then count after the #
+
+
+    """
+
+    def encode(self, strs: List[str]) -> str:
+        word = ""
+        for element in strs:
+            word += str(len(element)) + "#" + element
+        print(word)
+        return word
+
+    def decode(self, s: str) -> List[str]:
+        wordList = []
+        count = ""
+        index = 0
+        counting = True
+        for i in range(len(s)):
+        
+
+            if counting:
+                if s[i] == "#": 
+                    count = int(count)
+                    wordList.append("")
+                    counting = False
+                else:
+                    count += s[i]
+            else: #if not counting=
+                if count == 0:
+                    counting = True
+                    count = "" + s[i]
+                    index = 0
+                else:
+
+                    wordList[-1] += s[i]
+                    index += 1
+                    
+                    if index == count:
+                        counting = True
+                        count = ""
+                        index = 0
+                
+        return wordList
+
+
+            
+            
+            
