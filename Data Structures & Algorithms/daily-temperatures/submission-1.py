db0dfcbe66944d212ca7c6193c92599b4e4abcc8
Jan David Ella <117320525+JanDavidElla@users.noreply.github.com>
2026-08-30 15:41:05 -0700

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        """
+        We keep a stack,
+        basically it adds to the stack
+        if the stack is not empty, it will compare with current element
+
+        ex:
+        Input: temperatures = [30,38,30,36,35,40,28]
+
+        Output: [1,4,1,2,1,0,0]
+        """
+        output = [0] * len(temperatures)
+        stack = [] #will contain pairs [temp, index]
+
+        for i in range(len(temperatures)):
+            elem = temperatures[i]
+            while len(stack) != 0:
+                comp = stack.pop()
+                if comp[0] < elem:
+                    output[comp[1]] = i - comp[1]
+                else:
+                    stack.append(comp)
+                    break
+            stack.append([elem, i])
+        return output
+
