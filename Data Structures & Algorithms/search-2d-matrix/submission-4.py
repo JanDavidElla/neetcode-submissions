72adf4860f499870e3693f6436d5bbaf778b871a
Jan David Ella <117320525+JanDavidElla@users.noreply.github.com>
2026-08-31 22:57:17 -0700

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.py
@@ -0,0 +1,42 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        """
+        non-dereasing order
+
+        plan:
+        we can compare the first index of each matrix via binary search.
+        if left == right, then we look at that array and implement binary search.
+
+        [1,2],[3,4]
+        """
+
+        left = 0
+        right = len(matrix) - 1
+
+        while left < right:
+            mid = (left + right) // 2
+            if matrix[mid][0] == target:
+                return True
+            elif matrix[mid][0] < target:
+                if matrix[mid+1][0] > target: #edge case: before shifting right, it checks the rightest matrix's first element.
+                                                #if it is greater, then we can assume, mid is the one likely with the element.
+                    left = mid
+                    break
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        #reminder, if it is a regular 1D array
+        second_matrix = matrix[left] 
+        left = 0
+        right = len(second_matrix) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if second_matrix[mid] == target:
+                return True
+            elif second_matrix[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
+        #regular binary search
