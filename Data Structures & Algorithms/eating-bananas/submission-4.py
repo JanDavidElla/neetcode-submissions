11fac309a7b3e3c29e14ec669ab3d922f2cb2a92
Jan David Ella <117320525+JanDavidElla@users.noreply.github.com>
2026-09-01 13:06:05 -0700

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        """
+        ceil(x/k) = time
+
+        ceil(x/k) <= h
+
+        h = max number of hours you have to eat all bananas
+        k = minimum number bananas eaten per hour
+        """
+        piles = sorted(piles)
+
+        left = 1
+        right = max(piles)
+        mini = right
+
+        while left <= right:
+            mid = (left + right) // 2
+            time = 0
+            for i in range(len(piles)):
+                time += math.ceil(piles[i]/mid)
+            if time <= h:
+                if mid < mini:
+                    mini = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return mini
+
+ 
+        
