67ad97c2ec3c98d0c6b2c99d87ad88b19ea8bd04
Jan David Ella <117320525+JanDavidElla@users.noreply.github.com>
2026-08-31 20:20:22 -0700

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        """
+        car = sorted([[i,j] for position[i], position[j]])
+        in loop:
+        1. Add [0] by [1], remove boolean
+        2. Check:
+            - if >= target: remove
+            - elif k < len(car) - 1 and current >= prior:
+                    set to same as prior
+            - else
+        3. if remove then num += 1
+        [1,2,3]
+        [1,2]
+        """
+        cars = [[p, s] for p,s in zip(position, speed)]
+
+        stack = []
+        for p, s in sorted(cars)[::-1]:
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
